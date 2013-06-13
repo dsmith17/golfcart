b967d0fc754b00c7f7ab3a47dd2557bdf7f11cee
Luno89 <lunosnyper@gmail.com>
2013-06-13 06:00:38 -0400

    added script code

--- raspberry/controller.py
+++ raspberry/controller.py
@@ -2,17 +2,20 @@ from log import *
 import GPS
 import Arduino
 import Server
+import script
 
 url = 'http://157.182.184.52/~student1/command.php'
 # GPS_Port = 'COM17'
 GPS_Port = '/dev/ttyUSB0'
 # Arduino_Port = 'COM6'
 Arduino_Port = '/dev/ttyACM0'
+path = ''
 
 def  poll():
     Server.Ping()
     GPS.Check()
     Arduino.Check()
+    script.Check()
     time.sleep(0.55)
 
 # Ping the server once to get current Sequence
@@ -20,6 +23,7 @@ writeLog(LOG_ALWAYS, 'AGC Startup')
 Server.open(url)
 Arduino.open(Arduino_Port)
 GPS.open(GPS_Port)
+script.read(path)
 
 while(1):
     poll()
