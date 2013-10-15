bf9ca545d80a2b051c4446ff4705a02de9b936a0
Corigan Simpson <lunosnyper@gmail.com>
2013-10-15 20:30:19 +0000

    changeing script to read a string vs. a file

--- raspberry/script.py
+++ raspberry/script.py
@@ -4,20 +4,22 @@ import Arduino
 import Server
 import commands
 
-Script_File = ''
+Script_File = [] 
 Script_Running = False
+command_num = 0
 
 def start_script(script) :
     global Script_Running
     global Script_File
 
     #Script_File = (script, 'r')
-    Script_File = script
+    Script_File = script.splitlines()
     Script_Running = True
 
 def Check() :
     global Script_Running
     global Script_File
+    global command_num
 
     if not Script_Running :
         return
@@ -25,8 +27,10 @@ def Check() :
     if commands.Command_Running :
         return
 
-    command = Script_File.readline()
-    if Script_File.eof() :
+    command = Script_File[command_num]
+    command_num += 1
+    if len(Script_File) <= command_num :
+        command_num = 0
         Script_Running = False
         return
 
